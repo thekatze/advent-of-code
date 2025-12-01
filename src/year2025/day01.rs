17950c1736d828832285dfbe05627e2f8f429b00
Leo Katzengruber <leofel.katz@gmail.com>
2025-12-01 12:57:35 +0100

    2025-01 part 2

--- src/year2025/day01.rs
+++ src/year2025/day01.rs
@@ -1,64 +1,27 @@
 #[derive(Debug)]
 enum Rotation {
-    Left(u16),
-    Right(u16),
+    Left(i32),
+    Right(i32),
 }
 
-impl Rotation {
-    fn wrapping_rotate(&self, mut dial: &mut i32) -> u64 {
-        let amount: i32 = match self {
-            Rotation::Left(amount) => -(*amount as i32),
-            Rotation::Right(amount) => *amount as i32,
-        };
+fn try_parse_rotation(value: &str) -> Result<i32, ()> {
+    let (direction, amount) = value.split_at(1);
+    let amount: i32 = amount.parse().map_err(|_| ())?;
 
-        *dial += amount;
-
-        match dial {
-            (..0) => {
-                // TODO: solve arithmically
-                let mut wraps = 0;
-                while *dial < 0 {
-                    wraps += 1;
-                    *dial += 100;
-                }
-                wraps
-            }
-            (100..) => {
-                // TODO: solve arithmically
-                let mut wraps = 0;
-                while *dial >= 100 {
-                    wraps += 1;
-                    *dial -= 100;
-                }
-                wraps
-            }
-            _ => 0,
-        }
+    match direction {
+        "L" => Ok(-amount),
+        "R" => Ok(amount),
+        _ => Err(()),
     }
 }
 
-impl TryFrom<&str> for Rotation {
-    type Error = ();
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let (direction, amount) = value.split_at(1);
-        let amount = amount.parse().map_err(|_| ())?;
-
-        match direction {
-            "L" => Ok(Rotation::Left(amount)),
-            "R" => Ok(Rotation::Right(amount)),
-            _ => Err(()),
-        }
-    }
-}
-
-pub struct Parsed(Vec<Rotation>);
+pub struct Parsed(Vec<i32>);
 
 pub fn parse(input: &str) -> Parsed {
     Parsed(
         input
             .lines()
-            .map(|line| Rotation::try_from(line).expect("line not a rotation"))
+            .map(|line| try_parse_rotation(line).expect("line not a rotation"))
             .collect(),
     )
 }
@@ -70,14 +33,40 @@ pub fn part1(input: &Parsed) -> u64 {
         .0
         .iter()
         .filter(|rotation| {
-            rotation.wrapping_rotate(&mut dial);
-            dial == 0
+            dial += *rotation;
+            dial % 100 == 0
         })
         .count() as u64
 }
 
 pub fn part2(input: &Parsed) -> u64 {
-    0
+    let mut dial = 50;
+
+    input
+        .0
+        .iter()
+        .map(|rotation| {
+            let old_dial = dial;
+            let old_dial_f32 = dial as f32;
+            dial += *rotation;
+
+            let dial_f32 = dial as f32;
+
+            let mut clicks = ((old_dial_f32 / 100.0).floor() - ((dial_f32 / 100.0).floor())).abs();
+
+            // do click if we land on 0 from turning left
+            if rotation.is_negative() && dial % 100 == 0 {
+                clicks += 1.0;
+            }
+
+            // don't click if we start from 0 and turn left
+            if rotation.is_negative() && old_dial % 100 == 0 {
+                clicks -= 1.0;
+            }
+
+            clicks as u64
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -104,6 +93,30 @@ L82";
     #[test]
     fn part2() {
         let result = super::part2(&parse(SAMPLE_INPUT));
-        assert_eq!(result, 0)
+        assert_eq!(result, 6)
+    }
+
+    #[test]
+    fn counts_clicks_correctly() {
+        // Clicked 1 times rotating Left(24) from 24 to 0
+        // Clicked 1 times rotating Left(122) from 0 to 78
+
+        assert_eq!(
+            super::part2(&parse(
+                r"L26
+L24
+L122",
+            )),
+            2
+        );
+
+        // Clicked 2 times rotating Left(150) from 50 to 0
+        assert_eq!(super::part2(&parse(r"L150",)), 2);
+
+        // Clicked 1 time rotating Right(50) from 50 to 0
+        assert_eq!(super::part2(&parse(r"R50",)), 1);
+
+        // Clicked 1 time rotating Left(100) from 50 to 50
+        assert_eq!(super::part2(&parse(r"L50",)), 1);
     }
 }
